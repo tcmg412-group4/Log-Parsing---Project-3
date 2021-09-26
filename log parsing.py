ba513984cebf75d35fcdcbb14c80f3f6b147ce9b
GioHdz12 <90159706+GioHdz12@users.noreply.github.com>
2021-09-26 17:46:22 -0500

    Update log parsing.py
    Added the code to import the log file from the url and save it

--- log parsing.py
+++ log parsing.py
@@ -1,1 +1,10 @@
+# Beginning of code
+# Import to retrieve log file from url
+from urllib.request import urlretrieve
+# url
+url_path = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+# Name of the file file
+log_file = 'logs_file.log'
 
+# Copy and save file
+local_file, headers = urlretrieve(url_path, log_file)
