96dc872d5155972528cb86a03b2faaa264c2024c
h3nryn09 <90486871+h3nryn09@users.noreply.github.com>
2021-10-10 19:28:10 -0500

    Update log parsing.py

--- log parsing.py
+++ log parsing.py
@@ -52,3 +52,27 @@ print("Number of requests in the period:", n_requests)
 # Print if the file was successfully parsed or not
 print("Successfully parsed log file...")
 print()
+
+#5. what was the most requested file?
+import collections
+
+logfile = open("yourlogfile.log", "r")
+
+clean_log=[]
+
+for line in logfile:
+    try:
+        # copy the URLS to an empty list.
+        # We get the part between GET and HTTP
+        clean_log.append(line[line.index("GET")+4:line.index("HTTP")])
+    except:
+        pass
+
+counter = collections.Counter(clean_log)
+
+# get the Top 50 most popular URLs
+for count in counter.most_common(50):
+    print(str(count[1]) + "	" + str(count[0]))
+
+logfile.close()
+#6. What was the least requested file?
